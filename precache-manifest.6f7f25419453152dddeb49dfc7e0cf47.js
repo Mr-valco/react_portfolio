91f5430370109e7ddadc88a8bc20add0d786273f
Mr-valco <valentin.meica@gmail.com>
2020-03-24 22:50:01 -0400

    Updates

---
+++ precache-manifest.6f7f25419453152dddeb49dfc7e0cf47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d97e64dde6de981adaa9eefb60f0c4",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "00390cbd0096e2de7d04",
+    "url": "/react_portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f373ee9a91ed46c74f55",
+    "url": "/react_portfolio/static/js/2.77d46095.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.77d46095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00390cbd0096e2de7d04",
+    "url": "/react_portfolio/static/js/main.d970222f.chunk.js"
+  },
+  {
+    "revision": "a06b7793b710388c05e6",
+    "url": "/react_portfolio/static/js/runtime-main.b51f7317.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
